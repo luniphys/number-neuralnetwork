e669063c1f6c93e8d5e239c7a1a657d35bd8e07e
luniphys_lin <luniphys@gmail.com>
2026-01-06 12:13:36 +0100

    Added Function that automatically downloads MNIST data

--- neural.py
+++ neural.py
@@ -1,6 +1,48 @@
 import numpy as np
 import pandas as pd
 import random as rn
+import requests as req
+import os
+import shutil
+import zipfile as zipf
+
+
+
+def getData():
+    """
+    If the MNIST dataset is not on pc, this function will download and extract it
+    """
+
+    if not os.path.exists("MNIST"):
+        os.mkdir("MNIST")
+
+    if not os.path.isfile("MNIST/mnist_test.csv") or not os.path.isfile("MNIST/mnist_train.csv"):
+
+        def downloadData(name):
+            
+            url = f"https://raw.githubusercontent.com/phoebetronic/mnist/main/mnist_{name}.csv.zip"
+
+            response = req.get(url)
+            response.raise_for_status()
+
+            with open(f"MNIST/mnist_{name}.csv.zip", "wb") as file:
+                file.write(response.content)
+
+            with zipf.ZipFile(f"MNIST/mnist_{name}.csv.zip") as file:
+                file.extractall("MNIST")
+
+            os.remove(f"MNIST/mnist_{name}.csv.zip")
+
+
+        downloadData("test")
+        downloadData("train")
+
+        shutil.rmtree("MNIST/__MACOSX")
+
+
+getData()
+
+
 
 RES = 28 # 28x28 pixel canvas
 PIX_MAX = 255 # pixel strength 0-255
@@ -9,10 +51,10 @@ RAN_SIZE = 10 # interval [-RAN_SIZE, +RAN_SIZE] for initialization of weights & 
 OUT_SIZE = 10 # number of output neurons
 
 
-train = pd.read_csv('MNIST_CSV/mnist_train.csv', index_col=0, header=None) # index (first col) = drawn number, header (first row) = inc number 0-784 -> 10000x784
+train = pd.read_csv('MNIST/mnist_train.csv', index_col=0, header=None) # index (first col) = drawn number, header (first row) = inc number 0-784 -> 10000x784
 train = train/PIX_MAX # set scale 0-1
 
-test = pd.read_csv('MNIST_CSV/mnist_test.csv', index_col=0, header=None) # index (first col) = drawn number, header (first row) = inc number 0-784 -> 10000x784
+test = pd.read_csv('MNIST/mnist_test.csv', index_col=0, header=None) # index (first col) = drawn number, header (first row) = inc number 0-784 -> 10000x784
 test = test/PIX_MAX # set scale 0-1
 
 data = test
