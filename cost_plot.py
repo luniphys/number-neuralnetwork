9cbce5689fc832137f14652da44756bb0df4e2e8
luniphys-win <luniphys@gmail.com>
2026-02-01 14:21:18 +0100

    Cost plot jpg

--- cost_plot.py
+++ cost_plot.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-with open("cost.txt", "r") as file:
+with open("TrainedWBs/Trained_train/cost_train.txt", "r") as file:
     cost_lst = [float(line.strip()) for line in file]
 
 plt.plot(cost_lst)
 plt.ylabel("Cost")
 plt.xlabel("Training cycles")
 plt.grid()
+plt.savefig("cost_plot.jpg")
 plt.show()
